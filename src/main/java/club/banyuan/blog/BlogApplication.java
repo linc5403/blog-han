27a33dda6c351d3da7be8732e7e3903830d06895
linchuan <linc1982@icloud.com>
2020-05-07 11:52:01 +0800

    fix some dependencies

--- src/main/java/club/banyuan/blog/BlogApplication.java
+++ src/main/java/club/banyuan/blog/BlogApplication.java
@@ -1,26 +1,12 @@
 package club.banyuan.blog;
 
-import club.banyuan.blog.bean.Blog;
-import club.banyuan.blog.bean.Comment;
-import club.banyuan.blog.bean.User;
-import club.banyuan.blog.dao.BlogDao;
-import club.banyuan.blog.dao.CommentDao;
-import club.banyuan.blog.dao.UserDao;
-import club.banyuan.blog.service.MailService;
-import club.banyuan.blog.service.RedisService;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-
-import javax.mail.MessagingException;
-import java.util.List;
 
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) throws MessagingException {
+	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
     }
 }
