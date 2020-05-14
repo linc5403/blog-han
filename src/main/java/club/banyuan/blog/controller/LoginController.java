073be4df900d29b7ae919da5977f1e41c7ce8ce2
linchuan <linc1982@icloud.com>
2020-05-14 12:06:20 +0800

    add login form and service

--- src/main/java/club/banyuan/blog/controller/LoginController.java
+++ src/main/java/club/banyuan/blog/controller/LoginController.java
@@ -1,14 +1,45 @@
 package club.banyuan.blog.controller;
 
+import club.banyuan.blog.bean.User;
+import club.banyuan.blog.service.BlogService;
+import club.banyuan.blog.service.LoginService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @Autowired
+    private BlogService blogService;
+
     @GetMapping
     public String showLoginPage() {
         return "login";
+    }
+
+    @PostMapping
+    public String login(@RequestParam(name="username1") String username,
+                        @RequestParam String password,
+                        Model model
+    ) {
+        // 验证用户登录
+        User user = loginService.login(username, password);
+        if (user == null) {
+            return "login";
+        }
+        PageInfo info = blogService.findBlogs(user, 1, 10);
+
+        model.addAttribute("blogs", info);
+        return "admin-blogs";
     }
 }
